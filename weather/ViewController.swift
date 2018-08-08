03b9dc5b7f21597abcb98b4eea3d41a8c35c2adc
Bastian Aigner <basti@bastiaigner.com>
2018-08-08 02:13:14 +0200

    - Improved Storyboard design - Fixed bug: UI was updated from outside main thread - added "%" and "deg C" annotations to data labels

--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -22,6 +22,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
+        
         //parse json
         guard let url = URL(string: "http://api.sternwarte-ottilien.de/weather/current/") else {return}
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
@@ -60,8 +63,12 @@ class ViewController: UIViewController {
                                 let image = UIImage(data: imageData)
                                 
                                 //displaying the image
-                                self.allskyImage.image = image
                                 
+                                // we have to update the view in the main thread
+                                DispatchQueue.main.async {
+                                    self.allskyImage.image = image
+                                }
+    
                             } else {
                                 print("Image file is currupted")
                             }
@@ -78,16 +85,26 @@ class ViewController: UIViewController {
                 //set lables
                 let humidity = json["weatherData"]["humidity"].doubleValue
                 let temperature = json["weatherData"]["temperature"].doubleValue
-                let humidityAsString = String(format:"%.1f", humidity)
-                let temperatureAsString = String(format:"%.1f", temperature)
-                self.temperatureLabel.text = temperatureAsString
-                self.humidityLabel.text = humidityAsString
+                let humidityAsString = String(format:"%.1f", humidity) + "%"
+                let temperatureAsString = String(format:"%.1f℃", temperature)
+                
+                DispatchQueue.main.async {
+                    self.temperatureLabel.text = temperatureAsString
+                    self.humidityLabel.text = humidityAsString
+                }
             } catch let parsingError {
                 print("Error", parsingError)
             }
         }
         task.resume()
         
+        // setup allsky image view
+        self.allskyImage.clipsToBounds = true
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        self.allskyImage.layer.cornerRadius = self.allskyImage.frame.width / 2
     }
     
     override func didReceiveMemoryWarning() {
